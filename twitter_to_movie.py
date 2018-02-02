3fc7265b5fcd01efed477d7c8e5e86a2e82711ce
John Delaney <johnidel@aol.com>
2018-02-01 23:57:17 -0500

    FFMpeg implemented. Movies are made from as many tweets possible, with all temp files created and discarded in the working dir.

--- twitter_to_movie.py
+++ twitter_to_movie.py
@@ -2,6 +2,7 @@ import twitter
 import json
 import urllib.request
 import os
+import subprocess
 
 with open("keys.dat") as f:
 	keys = f.read().split()
@@ -10,7 +11,7 @@ api = twitter.Api(consumer_key= keys[0],
 			access_token_key=keys[2],
 			access_token_secret=keys[3])
 
-res = api.GetUserTimeline(screen_name="dannygarcia95", count=200, trim_user=True, exclude_replies=True)
+res = api.GetUserTimeline(screen_name="Omz2007", count=200, trim_user=True, exclude_replies=True)
 images = []
 for tweet in res:
 	js = tweet._json["entities"]
@@ -20,8 +21,24 @@ for tweet in res:
 				images.append(media["media_url"])
 
 for i in range(len(images)):
-	urllib.request.urlretrieve(images[i], "tmp_" + str(i).zfill(4) + ".jpg")
+	urllib.request.urlretrieve(images[i], "tmp_{}.jpg".format(str(i).zfill(4)))
 
 for i in range(len(images)):
-	os.remove("tmp_" + str(i).zfill(4) + ".jpg")
 
+	subprocess.call(('''ffmpeg -loop 1 -i tmp_{}.jpg -c:a libfdk_aac -ar 44100 -ac 2 -vf "scale='if(gt(a,16/9),1280,-1)':'if(gt(a,16/9),-1,720)',                                  pad=1280:720:(ow-iw)/2:(oh-ih)/2" -c:v libx264 -b:v 10M -pix_fmt yuv420p -r 30 -shortest -avoid_negative_ts make_zero -fflags +genpts -t 1 tmp_{}.mp4''').format(str(i).zfill(4) , str(i).zfill(4)),
+		cwd=os.path.dirname(os.path.realpath(__file__)), shell=True, env=dict(os.environ, PATH="C:/Users/johnidel/Downloads/ffmpeg-20180201-b1af0e2-win64-static/ffmpeg-20180201-b1af0e2-win64-static/bin"))
+
+with open("tmp_files.txt", "w") as f:
+	for i in range(len(images)):
+		f.write("file 'tmp_{}.mp4'\n".format(str(i).zfill(4)))
+
+
+subprocess.call("ffmpeg -f concat -i tmp_files.txt output.mp4",
+	cwd=os.path.dirname(os.path.realpath(__file__)),
+	shell=True,
+	env=dict(os.environ, PATH="C:/Users/johnidel/Downloads/ffmpeg-20180201-b1af0e2-win64-static/ffmpeg-20180201-b1af0e2-win64-static/bin"))
+		
+
+for i in range(len(images)):
+	os.remove("tmp_{}.jpg".format(str(i).zfill(4)))
+	os.remove("tmp_{}.mp4".format(str(i).zfill(4)))
